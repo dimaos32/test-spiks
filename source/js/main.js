f7620768eab472a5a3a6f05fad300affe07c0da9
Dima Os <dimaos@proton.me>
2024-02-14 22:54:11 +0300

    Remove unusing components

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-import { initModals } from './modules/modals/init-modals';
+// import { initModals } from './modules/modals/init-modals';
 
 // ---------------------------------
 
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    // initModals();
   });
 });
 
