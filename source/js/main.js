7810bccaea4a053959761ac8153f5523a2c47ba2
Dima Os <dimaos@proton.me>
2024-02-17 23:13:04 +0300

    Optimize js

--- source/js/main.js
+++ source/js/main.js
@@ -15,12 +15,14 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  initRangeSliders();
+  initSelects();
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initRangeSliders();
-    initSelects();
-  });
+  // window.addEventListener('load', () => {
+
+  // });
 });
 
 // ---------------------------------
